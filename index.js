3b10fd724ae969bff88b033d1136e25b65f34a2c
dlee16 <diana.lee116@gmail.com>
2019-04-11 18:25:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(findName => {
+        return findName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, name){
+    let beginningName = name.length; 
+    return drivers.filter(findName => {
+        return findName.slice(0, beginningName) === name
+    })
+}
+
+
+function matchName(drivers, name) {
+    return drivers.filter(findName => {
+        return findName.name.toLowerCase() === name.toLowerCase();
+    });
+}
